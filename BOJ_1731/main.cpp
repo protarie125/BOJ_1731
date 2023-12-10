88c2404196fd03d39beabde4c4188f6bfe2bdae2
protarie125 <protarie@gmail.com>
2023-12-10 12:31:33 +0900

    초기 커밋.

---
+++ BOJ_1731/main.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  int last = c;
+  for (auto i = 3; i < n; ++i) {
+    cin >> last;
+  }
+
+  if (b - a == c - b) {
+    cout << last + b - a;
+  } else {
+    cout << last * (b / a);
+  }
+
+  return 0;
+}
